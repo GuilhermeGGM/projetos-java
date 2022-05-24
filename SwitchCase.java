0f66803181cbc128795d135cc4a30df963428788
guilherme.moreira <guigui36812@gmail.com>
2022-05-24 09:04:02 -0300

    README.md

---
+++ SwitchCase.java
@@ -0,0 +1,20 @@
+public class SwitchCase {
+    public static void main(String[] args){
+        int mes = 2;
+
+        switch (mes) {
+
+            case 1:
+                System.out.println("mês 1");
+                break;
+
+            case 2:
+                System.out.println("mês 2");
+                break;
+            
+            default:
+                System.out.println("Não é mês 1 ou 2");
+
+        }
+    }    
+}
