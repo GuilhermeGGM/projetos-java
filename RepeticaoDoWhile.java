c8e395ba182e6be7d5a33059b1fce6b35c1e0b00
guilherme.moreira <guigui36812@gmail.com>
2022-05-24 09:19:14 -0300

    Treinamento java

---
+++ RepeticaoDoWhile.java
@@ -0,0 +1,9 @@
+public class RepeticaoDoWhile{
+    public static void main(String[] args){
+        int i = 0;
+        do {
+            System.out.println("Olá mundo");
+            i++;
+        } while(i < 10);
+    }
+}
